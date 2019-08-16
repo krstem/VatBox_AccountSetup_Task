db5185594bdfc7357b5859b8b3fdcd0fe3023fb1
Ines <inesmilanova1@gmail.com>
2019-08-16 13:50:13 +0200

    header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,9 @@ import Icon from './Icon';
 const CLASS_NAMES = {
     HEADER_CONTAINER: 'header-container',
     HEADER_CONTENT: 'header-content',
-    USERNAME_ICON: 'username-icon'
+    USERNAME_ICON: 'username-icon',
+    HEADER_ITEM_CONTAINER: 'header-item-container',
+    COMPANY_NAME: 'company_name'
 }
 
 class Header extends Component {
@@ -13,12 +15,15 @@ class Header extends Component {
         return (
             <div className={CLASS_NAMES.HEADER_CONTAINER}>
                 <div className={CLASS_NAMES.HEADER_CONTENT}>
-                    <div>
+                    <div className={CLASS_NAMES.HEADER_ITEM_CONTAINER}>
                         <span className={CLASS_NAMES.USERNAME_ICON}>
                             <Icon icon='user.svg' />
-                        </span> UserName </div>
-                    <div>Manager Icon</div>
-                    <div>Company Name</div>
+                        </span> UserName
+                    </div>
+                    <div className={CLASS_NAMES.HEADER_ITEM_CONTAINER}>Manager
+                        <Icon icon='arrow-down.svg' />
+                    </div>
+                    <div className={CLASS_NAMES.COMPANY_NAME}>Company Name</div>
                 </div>
             </div>
         );

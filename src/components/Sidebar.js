96c6db3bb24e4fc1b69401f5bca7ad1ae995337c
Angela Atanasova <atanasovaangela93@gmail.com>
2019-08-20 11:12:53 +0200

    sidebar finished

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Icon from './Icon';
+import cx from 'classnames';
 
 const CLASS_NAMES = {
   SIDEBAR_WRAPPER: 'nav-side-menu',
   SIDEBAR_MENU: 'menu-list',
   SIDEBAR_MENU_ITEM: 'sidebar-menu-item',
-  SIDEBAR_ACTIVE_MENU_ITEM: 'sidebar-active-menu-item'
+  SIDEBAR_MENU_LOGO: 'sidebar-menu-logo',
+  SIDEBAR_ACTIVE_MENU_ITEM: 'sidebar-active-menu-item',
+  SIDEBAR_ICON_SIZE: 'sidebar-icon-size',
+  SIDEBAR_DISABLED_MENU_ITEM: 'sidebar-disabled-item',
+  SIDEBAR_SUPPORT_BUTTON: 'sidebar-support-button',
+  SIDEBAR_BUTTON_NAME: 'sidebar-button-name',
+  SIDEBAR_NO_ICON_IN_LIST: 'no-icon-in-list'
 };
 
   const VatRecovery = [
@@ -19,8 +26,6 @@ const CLASS_NAMES = {
   ]
     
   
-
-
 class Sidebar extends React.Component {
     constructor() {
       super();
@@ -30,60 +35,70 @@ class Sidebar extends React.Component {
     render() { 
       return(
         <div class={CLASS_NAMES.SIDEBAR_WRAPPER}>
-            <div class="brand">
+            <div class={CLASS_NAMES.SIDEBAR_MENU_LOGO}>
               <Icon icon='logo.png'/>
             </div>
           
                 <div class={CLASS_NAMES.SIDEBAR_MENU}>
                     <ul>
                         <li>
-                          <a href="#" className="icon-size">
+                          <a href="#" className={CLASS_NAMES.SIDEBAR_ICON_SIZE}>
                           <Icon icon='menu.svg' /> VAT Recovery
                           </a>
                         </li>
-                        <ul class="sub-menu collapse isDisabled">
+                        <ul>
                           {VatRecovery.map(item => {
-                            return <li className='no-icon-list'>{item.name}</li>;
+                            return <li className={CLASS_NAMES.SIDEBAR_NO_ICON_IN_LIST}>
+                                      <a href="#" className={CLASS_NAMES.SIDEBAR_DISABLED_MENU_ITEM}>{item.name}</a>
+                                   </li>;
                           })}
                         </ul>
 
                         <li>
-                          <a href="#" className='icon-size'> <Icon icon='bill.svg' />Evidance Qualification</a>
+                          <a href="#" className={CLASS_NAMES.SIDEBAR_ICON_SIZE}> 
+                            <Icon icon='bill.svg' />
+                            Evidance Qualification
+                          </a>
                         </li>
-                        <ul class="isDisabled">
+                        <ul>
                           {EvidanceQualification.map(item => {
-                            return <li className='no-icon-list'>{item.name}</li>;
+                            return <li className={CLASS_NAMES.SIDEBAR_NO_ICON_IN_LIST}>
+                                      <a href="#" className={CLASS_NAMES.SIDEBAR_DISABLED_MENU_ITEM}>{item.name}</a>
+                                   </li>;
                           })}
                         </ul>
 
                         <li>
-                          <a href="#" className='icon-size'> <Icon icon='ruler.svg' />TAX Tailor</a>
+                          <a href="#" className={CLASS_NAMES.SIDEBAR_ICON_SIZE}> <Icon icon='ruler.svg' />TAX Tailor</a>
                         </li> 
-                        <ul class="isDisabled">
+                        <ul>
                           {TAXTailor.map(item => {
                             return <li>
-                                      <span className='icon-size'>
-                                        <Icon icon={item.icon} />
-                                      </span>
-                                      {item.name}
-                                    </li>
+                                      <a href="#" class={cx(CLASS_NAMES.SIDEBAR_ICON_SIZE , CLASS_NAMES.SIDEBAR_DISABLED_MENU_ITEM)}>
+                                          <Icon icon={item.icon} />
+                                          {item.name}
+                                      </a>
+                                  </li>
                           })}
                         </ul> 
 
                         <li>
-                          <div className="account-setup">
-                            <a href="#" className='icon-size'><Icon icon='rectangle.svg'/>Account Setup</a>
+                          <div className={CLASS_NAMES.SIDEBAR_ACTIVE_MENU_ITEM}>
+                            <a href="#" className={CLASS_NAMES.SIDEBAR_ICON_SIZE}>
+                              <Icon icon='rectangle.svg'/>
+                              Account Setup
+                            </a>
                           </div>
                         </li>
                     </ul>
             </div>
-            <button className='support-button'>
-                <span className='icon-size'>
-                  {/* <Icon icon='support.svg'/> */}
-                </span>
-                <span className="button-name">
+            <button className={CLASS_NAMES.SIDEBAR_SUPPORT_BUTTON}>
+                <a href="#" className={CLASS_NAMES.SIDEBAR_ICON_SIZE}>
+                  <Icon icon='support.svg'/>
+                  <span className={CLASS_NAMES.SIDEBAR_BUTTON_NAME}>
                     VATBox Support
-                </span>
+                  </span>
+                </a>
             </button>
         </div>
       );

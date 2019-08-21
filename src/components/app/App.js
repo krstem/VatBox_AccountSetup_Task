4eacc63c68df53f35aa95906e0b3aa71219801fb
Ines <inesmilanova1@gmail.com>
2019-08-21 10:32:27 +0200

    App component layout change

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,25 +1,29 @@
 import React, { useReducer } from 'react';
-import './App.css';
+import './App.scss';
 import { initialState } from '../../services/constants';
 import LeadersContext from '../../services/leadersContext';
 import accountSetupReducer from '../../services/accountSetupReducer';
 
 import Header from '../Header';
+import Sidebar from '../Sidebar';
 import AccountSetup from '../account-setup/AccountSetup';
 import ProgressBarContainer from '../progressBar/progressBarContainer';
 
 const App = () => {
 
   const [data, dispatch] = useReducer(accountSetupReducer, initialState);
-  import Sidebar from '../Sidebar';
 
   return (
     <div className="App">
       <LeadersContext.Provider value={{ data, dispatch }}>
         <Header />
-        <Sidebar />
-        <AccountSetup />
-        <ProgressBarContainer />
+        <div className='content-wrapper'>
+          <Sidebar />
+          <div className='right-section'>
+            <AccountSetup />
+            <ProgressBarContainer />
+          </div>
+        </div>
       </LeadersContext.Provider>
     </div>
   );

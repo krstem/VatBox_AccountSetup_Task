ffa392d02f97191bb1a20a4cd9184f7e07a4abb9
Ines <inesmilanova1@gmail.com>
2019-08-23 13:25:49 +0200

    functional slider -- refactor in progress

--- src/components/progressBar/actionSlider.js
+++ src/components/progressBar/actionSlider.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import cx from 'classnames';
 
 import Icon from '../Icon';
 
@@ -14,8 +15,20 @@ const CLASS_NAMES = {
     SLIDING_ELEMENT: 'sliding-element'
 }
 
-function addAnimationClass() {
-    console.log('test', test);
+function slideRight() {
+    const firstElement = document.getElementsByClassName('sliding-element-1')[0];
+    const secondElement = document.getElementsByClassName('sliding-element-2')[0];
+    firstElement.style.left = `-558px`;
+    secondElement.style.left = `0`;
+    console.log('right');
+}
+
+function slideLeft() {
+    const firstElement = document.getElementsByClassName('sliding-element-1')[0];
+    const secondElement = document.getElementsByClassName('sliding-element-2')[0];
+    secondElement.style.left = `-${558 * 2}px`;
+    firstElement.style.left = `0`;
+    console.log('left');
 }
 
 const ActionSlider = (props) => {
@@ -23,12 +36,12 @@ const ActionSlider = (props) => {
     return (
         <div className={CLASS_NAMES.SLIDER_CONTAINER}>
             {/* sliding div */}
-            <div className={CLASS_NAMES.SLIDING_ELEMENT}>
+            <div className={cx(CLASS_NAMES.SLIDING_ELEMENT, `${CLASS_NAMES.SLIDING_ELEMENT}-1`)}>
                 <div className={CLASS_NAMES.SLIDER_HEADER}>
                     <div className={CLASS_NAMES.SLIDER_TITLE}>Call for Action #1</div>
                     <div className={CLASS_NAMES.SLIDER_ICONS_WRAPPER}>
-                        <div className={CLASS_NAMES.SLIDER_LEFT_ARROW}> <Icon icon={'arrow-down.svg'} /> </div>
-                        <div className={CLASS_NAMES.SLIDER_RIGHT_ARROW}> <Icon icon={'arrow-down.svg'} /> </div>
+                        <div className={CLASS_NAMES.SLIDER_RIGHT_ARROW}
+                            onClick={slideRight}> <Icon icon={'arrow-down.svg'} /> </div>
                     </div>
                 </div>
                 <p className={CLASS_NAMES.SLIDER_TEXT}>Exiting the method early if we are at the end of the images array.
@@ -38,20 +51,18 @@ const ActionSlider = (props) => {
             {/* end sliding div */}
 
             {/* sliding div */}
-            <div className={CLASS_NAMES.SLIDING_ELEMENT}>
+            <div className={cx(CLASS_NAMES.SLIDING_ELEMENT, `${CLASS_NAMES.SLIDING_ELEMENT}-2`)}>
                 <div className={CLASS_NAMES.SLIDER_HEADER}>
                     <div className={CLASS_NAMES.SLIDER_TITLE}>Call for Action #2</div>
                     <div className={CLASS_NAMES.SLIDER_ICONS_WRAPPER}>
-                        <div className={CLASS_NAMES.SLIDER_LEFT_ARROW}>
-                            <Icon icon={'arrow-down.svg'} />
-                        </div>
-                        <div className={CLASS_NAMES.SLIDER_RIGHT_ARROW}>
+                        <div className={CLASS_NAMES.SLIDER_LEFT_ARROW}
+                            onClick={slideLeft}>
                             <Icon icon={'arrow-down.svg'} />
                         </div>
                     </div>
                 </div>
-                <p className={CLASS_NAMES.SLIDER_TEXT}>Exiting the method early if we are at the end of the images array.
-               We also want to reset currentIndex and translateValue, so we return to the first image in the array.</p>
+                <p className={CLASS_NAMES.SLIDER_TEXT}>Lorem Ipsum: usage Lorem ipsum is a pseudo-Latin text used in web design,
+                typography, layout, and printing in place of English to emphasise design elements over content.</p>
                 <div>Lorem ipsum now</div>
             </div>
             {/* end sliding div */}

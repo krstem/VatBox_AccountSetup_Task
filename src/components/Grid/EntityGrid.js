443391dd3da179dba5c0b27139f49e220f14ff77
Ines <inesmilanova1@gmail.com>
2019-08-26 09:47:49 +0200

    removed console.log in table

--- src/components/Grid/EntityGrid.js
+++ src/components/Grid/EntityGrid.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import ReactDataGrid from 'react-data-grid';
 import Icon from '../Icon';
 import entityGridSchema from './entityGridSchema'
-import {rowGetter, tableData} from './TableData';
-import {AutoSizer} from 'react-virtualized';
+import { rowGetter, tableData } from './TableData';
+import { AutoSizer } from 'react-virtualized';
 
 const CLASS_NAMES = {
     REACT_GRID_CHECKBOX_CONTAINER: 'react-grid-checkbox-container',
@@ -16,32 +16,30 @@ function EntityGrid() {
     const onRowsSelected = rows => {
         let rowIds = rows.map(r => r.row.id);
         setSelectedGroupIds([...selectedGroupIds, ...rowIds]);
-      };
-    
+    };
+
     const onRowsDeselected = rows => {
         let rowIds = rows.map(r => r.row.id);
         let selected = selectedGroupIds.filter(i => rowIds.indexOf(i) === -1)
 
         setSelectedGroupIds(selected);
-      };
+    };
 
-      console.log("Selected Group", selectedGroupIds)
-    
     return (<ReactDataGrid columns={entityGridSchema}
-                            rowGetter={rowGetter}
-                            rowsCount={tableData.length}
-                            rowHeight={40}
-                            headerRowHeight={41}
-                            minHeight={500} 
-                            rowSelection={{
-                                showCheckbox: true,
-                                onRowsSelected: onRowsSelected,
-                                onRowsDeselected: onRowsDeselected,
-                                selectBy: {
-                                    keys: {rowKey: 'id', values: selectedGroupIds}
-                                }
-                            }}
-            />);
+        rowGetter={rowGetter}
+        rowsCount={tableData.length}
+        rowHeight={40}
+        headerRowHeight={41}
+        minHeight={500}
+        rowSelection={{
+            showCheckbox: true,
+            onRowsSelected: onRowsSelected,
+            onRowsDeselected: onRowsDeselected,
+            selectBy: {
+                keys: { rowKey: 'id', values: selectedGroupIds }
+            }
+        }}
+    />);
 }
 
 export default EntityGrid;
